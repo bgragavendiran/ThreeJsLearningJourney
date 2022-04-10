b56459bcff9f2cb9591068a5e0446765548e2d2e
Ragavendiran <36047046+RAGANITHI@users.noreply.github.com>
2022-04-10 17:17:17 +0530

    with WebPack

---
+++ starterCode/src/script.js
@@ -0,0 +1,35 @@
+import './style.css'
+import * as THREE from 'three'
+console.log(THREE);
+const canvas=document.querySelector('.webgl')
+
+// Scene
+const Scene= new THREE.Scene()
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color:'#ff0000'})
+
+const cube = new THREE.Mesh(geometry,material)
+cube.position.x=0.7
+cube.position.y=-0.6
+cube.position.z=0
+
+Scene.add(cube)
+
+console.log(cube.position.length())
+
+//Sizes
+const Sizes={
+    width: 800,
+    height:600
+}
+//Camera
+const camera = new THREE.PerspectiveCamera(75,Sizes.width/Sizes.height)
+camera.position.z=3
+
+
+Scene.add(camera)
+
+//renderer
+const renderer =new THREE.WebGLRenderer(
